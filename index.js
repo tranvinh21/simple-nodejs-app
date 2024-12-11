082d3dab565957d44f098a0b6e089945360674b3
Trần Vinh <97041326+tranvinh21@users.noreply.github.com>
2024-12-11 11:15:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000;
 
 // Define a route for the root URL ('/')
 app.get('/', (req, res) => {
-  res.send('Hello, World! , I\'m Vinh');
+  res.send('Hello, World! , I\'m Vinh and i am 20 yrs');
 });
 
 // Start the server
